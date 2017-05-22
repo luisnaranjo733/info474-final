2eb6ec2f7d33df4f876f74e47d2e997328ec4f88
Luis Naranjo <luisnaranjo733@gmail.com>
2017-05-21 20:00:26 -0700

    add input event handling and refactor existing code
    I added input event handling for the buttons, selects, and toggles and i
    added corresponding global variables and some starter state handling
    code.
    
    I also refactored the table constants out of the on DOM load callback

--- js/main.js
+++ js/main.js
@@ -1,62 +1,80 @@
-$(function() {
+// define the table types and their areas in pixels
+let TABLE_SIZE = {
+    one: {
+        width: 100,
+        height: 100,
+    },
+    two: {
+        width: 200,
+        height: 100,
+    },
+    three: {
+        width: 300,
+        height: 100,
+    }
+}
 
-    $('#about').click(() => {
-        let options = {};
-        $('#myModal').modal(options)
+// define the restaurant layout in terms of (x,y) pixel anchor points and table types
+let TABLE_LAYOUT = [
+    {
+        x: 525,
+        y: 250,
+        type: TABLE_SIZE.one
+    },
+    {
+        x: 650,
+        y: 250,
+        type: TABLE_SIZE.one
+    },
+    {
+        x: 1100,
+        y: 100,
+        type: TABLE_SIZE.two
+    },
+    {
+        x: 800,
+        y: 575,
+        type: TABLE_SIZE.two
+    },
+    {
+        x: 1100,
+        y: 350,
+        type: TABLE_SIZE.three
+    }
+];
+
+let DEFAULT_TABLE_FILL = 'grey';
+
+$(function () {
+
+    let party_pattern = 'Random';
+
+    // open bootstrap modal to display about section on click
+    $('#about').click(() => $('#myModal').modal());
+
+    // update party_pattern global var on select change
+    $('select').change(() => {
+        party_pattern = $('select').val();
+        console.log(`Change the party pattern to ${party_pattern} and reset the queue`);
     });
 
-    let table_size = {
-        one: {
-            width: 100,
-            height: 100,
-        },
-        two: {
-            width: 200,
-            height: 100, 
-        },
-        three: {
-            width: 300,
-            height: 100, 
-        }
-    }
-
-    let table_data = [
-        {
-            x: 525,
-            y: 250,
-            type: table_size.one
-        },
-        {
-            x: 650,
-            y: 250,
-            type: table_size.one
-        },
-        {
-            x: 1100,
-            y: 100,
-            type: table_size.two
-        },
-        {
-            x: 800,
-            y: 575,
-            type: table_size.two
-        },
-        {
-            x: 1100,
-            y: 350,
-            type: table_size.three
-        }
-    ];
+    // handle stepping through the algorithm
+    $('#step-btn').click(() => {
+        // pop a party off the queue and seat them according to the algorithm
+        let algorithm_is_enabled = $('#algorithm-enabled').is(':checked');
+        console.log(`Place the next party in the queue`);
+        console.log(`algorithm_is_enabled=${algorithm_is_enabled} and party_pattern=${party_pattern}`);
+    });
 
     let svg = d3.select('#main-svg');
-    let tables = svg.selectAll('rect').data(table_data);
+    let tables = svg.selectAll('rect').data(TABLE_LAYOUT);
 
     tables.enter().append('rect')
         .attr('x', table => table.x)
         .attr('y', table => table.y)
         .attr('width', table => table.type.width)
         .attr('height', table => table.type.height)
-        .attr('fill', 'blue')
+        .attr('fill', DEFAULT_TABLE_FILL)
 
 
 });
