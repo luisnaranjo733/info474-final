94e919a856dae5bc5233f4ba0f33f6491e63b28c
Jooneil Ahn <jooneil.ahn@faira.com>
2017-05-22 18:48:41 -0700

    updated layout to draw tables in the proper locations based on the width of the left-pane div.

--- js/layout.js
+++ js/layout.js
@@ -45,143 +45,152 @@ We can also include parent table data on each seat if that helps.
 
 */
 
-// define the table types and their areas in pixels
-// each seat is defined relative to the top left corner of the table.
-let TABLE_TYPE = {
-    one: {
-        name: '1+1',
-        width: 75,
-        height: 75,
-        seats: [
-            {
-                x: 37.5,
-                y: 0
-            },
-            {
-                x: 37.5,
-                y: 75
-            }
-        ]
-    },
-    two: {
-        name: '2+2',
-        width: 150,
-        height: 75,
-        seats: [
-            {
-                x: 37.5,
-                y: 0
-            },
-            {
-                x: 112.5,
-                y: 0
-            },
-            {
-                x: 37.5,
-                y: 75
-            },
-            {
-                x: 112.5,
-                y: 75
-            }
-        ]
-    },
-    three: {
-        name: '3+3',
-        width: 225,
-        height: 75,
-        seats: [
-            {
-                x: 37.5,
-                y: 0
-            },
-            {
-                x: 112.5,
-                y: 0
-            },
-            {
-                x: 187.5,
-                y: 0
-            },
-
-            {
-                x: 37.5,
-                y: 75
-            },
-            {
-                x: 112.5,
-                y: 75
-            },
-            {
-                x: 187.5,
-                y: 75
-            },
-        ]
-    }
-};
-
-// define the table layout in terms of (x,y) pixel anchor points and table types
-// (0, 0) is the top left corner of the svg NOT the window.
-// This means that a table placed at (0, 0) would actually end up at (0, 125) since the
-// navbar and toolbar are 125 pixels tall
-let TABLE_LAYOUT = [
-    {
-        x: 443.75,
-        y: 187.5,
-        type: TABLE_TYPE.one
-    },
-    {
-        x: 537.5,
-        y: 187.5,
-        type: TABLE_TYPE.one
-    },
-    {
-        x: 875,
-        y: 75,
-        type: TABLE_TYPE.two
-    },
-    {
-        x: 650,
-        y: 431.25,
-        type: TABLE_TYPE.two
-    },
-    {
-        x: 875,
-        y: 262.5,
-        type: TABLE_TYPE.three
-    }
-];
-
 let TABLES = [];
 let SEATS = [];
 
-let _table_count = 0;
-let _seat_count = 0;
+$(document).ready(function()
+{
+    // define the table types and their areas in pixels
+    // each seat is defined relative to the top left corner of the table.
+    let TABLE_TYPE = {
+        one: {
+            name: '1+1',
+            width: 75,
+            height: 75,
+            seats: [
+                {
+                    x: 37.5,
+                    y: 0
+                },
+                {
+                    x: 37.5,
+                    y: 75
+                }
+            ]
+        },
+        two: {
+            name: '2+2',
+            width: 150,
+            height: 75,
+            seats: [
+                {
+                    x: 37.5,
+                    y: 0
+                },
+                {
+                    x: 112.5,
+                    y: 0
+                },
+                {
+                    x: 37.5,
+                    y: 75
+                },
+                {
+                    x: 112.5,
+                    y: 75
+                }
+            ]
+        },
+        three: {
+            name: '3+3',
+            width: 225,
+            height: 75,
+            seats: [
+                {
+                    x: 37.5,
+                    y: 0
+                },
+                {
+                    x: 112.5,
+                    y: 0
+                },
+                {
+                    x: 187.5,
+                    y: 0
+                },
 
-TABLES = TABLE_LAYOUT.map(table => {
-    let absolute_seats = [];
-    table.type.seats.forEach(relative_seat => {
-        let absolute_seat = {
-            seat_id: _seat_count,
-            seat_x: table.x + relative_seat.x,
-            seat_y: table.y + relative_seat.y,
-            table_type: table.type.name
-        };
-        _seat_count += 1;
-        absolute_seats.push(absolute_seat);
-        SEATS.push(absolute_seat);
-    });
-
-    table = {
-        table_id: _table_count,
-        table_type: table.type.name,
-        table_x: table.x,
-        table_y: table.y,
-        table_width: table.type.width,
-        table_height: table.type.height,
-
-        seats: absolute_seats
+                {
+                    x: 37.5,
+                    y: 75
+                },
+                {
+                    x: 112.5,
+                    y: 75
+                },
+                {
+                    x: 187.5,
+                    y: 75
+                },
+            ]
+        }
     };
-    _table_count += 1;
-    return table;
+
+    // get the width of the left div in order to ensure that tables get drawn in the
+    // right div
+    var left_div_width = $('#left-pane').width();
+
+    // define the table layout in terms of (x,y) pixel anchor points and table types
+    // (0, 0) is the top left corner of the svg NOT the window.
+    // This means that a table placed at (0, 0) would actually end up at (0, 125) since the
+    // navbar and toolbar are 125 pixels tall
+    let TABLE_LAYOUT = [
+        {
+            x: left_div_width + 70,
+            y: 187.5,
+            type: TABLE_TYPE.one
+        },
+        {
+            x: left_div_width + 175,
+            y: 187.5,
+            type: TABLE_TYPE.one
+        },
+        {
+            x: left_div_width + 490,
+            y: 75,
+            type: TABLE_TYPE.two
+        },
+        {
+            x: left_div_width + 290,
+            y: 431.25,
+            type: TABLE_TYPE.two
+        },
+        {
+            x: left_div_width + 490,
+            y: 262.5,
+            type: TABLE_TYPE.three
+        }
+    ];
+
+
+
+    let _table_count = 0;
+    let _seat_count = 0;
+
+    TABLES = TABLE_LAYOUT.map(table => {
+        let absolute_seats = [];
+        table.type.seats.forEach(relative_seat => {
+            let absolute_seat = {
+                seat_id: _seat_count,
+                seat_x: table.x + relative_seat.x,
+                seat_y: table.y + relative_seat.y,
+                table_type: table.type.name
+            };
+            _seat_count += 1;
+            absolute_seats.push(absolute_seat);
+            SEATS.push(absolute_seat);
+        });
+
+        table = {
+            table_id: _table_count,
+            table_type: table.type.name,
+            table_x: table.x,
+            table_y: table.y,
+            table_width: table.type.width,
+            table_height: table.type.height,
+
+            seats: absolute_seats
+        };
+        _table_count += 1;
+        return table;
+    });
 });
